45f5ad15a7942e3f25fa9ea91231828469969195
gotr00t0day <48636787+gotr00t0day@users.noreply.github.com>
2019-06-12 00:45:42 -0400

    Create vulnbanner.py

---
+++ vulnbanner.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import socket 
+import os, sys
+
+# Vulnerable banner detection script - c0deninja
+
+def getbanner(ip, port):
+    try:
+        socket.setdefaulttimeout(2)
+        sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sck.connect((ip,port))
+        banner = sck.recv(1024)
+        return banner
+    except:
+        return
+
+def vulncheck(banner, filename):
+    f = open(filename, "r")
+    for line in f.readlines():
+        if line.strip("\n").encode() in banner:
+            print ("Vulnerable: {}".format(banner.strip(b"\n\r")))
+    
+def main():
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        if not os.path.isfile(filename):
+            print ("File doesn't exist")
+            exit(0)
+        if not os.access(filename, os.R_OK):
+            print ("Access Denied")
+            exit(0)
+    else:
+        print ("Usage: " + str(sys.argv[0]) + " File.txt")
+        exit(0)
+    for port in range(1,80):
+        ip = "10.0.0.221"
+        banner = getbanner(ip, port)
+        if banner:
+            print ("{}/{} : {}".format(ip, port, banner))
+            vulncheck(banner, filename)
+main()
